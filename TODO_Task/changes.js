9461176535c7fc2370c01362e8f7bcd66f083c01
Darshan Kagathara <107626828+Darshan-Weybee@users.noreply.github.com>
2022-07-25 09:56:29 +0530

    Delete changes.js

--- TODO_Task/changes.js
+++
@@ -1,6 +0,0 @@
-// edit and delete button
-// uncheckbox process
-// allbtn  innerHTML
-//toggle css
-// first search button then go on add button ////
-// directly add task in the add button for loop problem
